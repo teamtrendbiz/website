dad923bc0cc374f0c6ccb7cd24edb0784b5e397c
teamtrendbiz <103359682+teamtrendbiz@users.noreply.github.com>
2022-04-12 17:25:17 +0530

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,7 +6,7 @@ var servicesVAr = 0;
 
 function toggleMenu(){
     if(navVar%2 == 0){
-        navBar.style.height = "40vh";
+        navBar.style.height = "45vh";
         menu.style.display = "flex";
         navVar = 1;
     }
